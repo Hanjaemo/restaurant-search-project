fe2f6b1df929e08f5b4caf092d9e0cafb6479acd
Hanjaemo <hjm2530@gmail.com>
2023-03-10 17:00:10 +0900

    Initial commit

---
+++ src/main/java/project/restaurant/web/controller/BasicController.java
@@ -0,0 +1,12 @@
+package project.restaurant.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/basic/test")
+public class BasicController {
+
+    
+}
