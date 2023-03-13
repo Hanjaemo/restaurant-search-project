8f6903df91d89659330232a470182655584e3dd3
Hanjaemo <hjm2530@gmail.com>
2023-03-14 01:00:27 +0900

    commit

--- src/main/java/project/restaurant/domain/User.java
+++ src/main/java/project/restaurant/domain/User.java
@@ -2,13 +2,17 @@ package project.restaurant.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Setter
 @Getter
+@Component
 public class User {
 
     private String name;
-    private Integer location;
+    private List<Integer> locationList;
     private Boolean single;
     private String foodType;
     private String category;
@@ -17,9 +21,9 @@ public class User {
     public User() {
     }
 
-    public User(String name, Integer location, Boolean single, String foodType, String category, Integer price) {
+    public User(String name, List<Integer> locationList, Boolean single, String foodType, String category, Integer price) {
         this.name = name;
-        this.location = location;
+        this.locationList = locationList;
         this.single = single;
         this.foodType = foodType;
         this.category = category;
--- src/main/java/project/restaurant/web/controller/BasicController.java
+++ src/main/java/project/restaurant/web/controller/BasicController.java
@@ -4,20 +4,43 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import project.restaurant.repository.RestSearchCond;
+import project.restaurant.domain.User;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
 @RequestMapping()
+@RequiredArgsConstructor
 public class BasicController {
+
+    private final User user;
 
     @GetMapping("/location")
     public String location() {
         return "main/location";
     }
 
+/*
+    @PostMapping("/single")
+    public String single(@ModelAttribute List<Integer> location, Model model) {
+        user.setLocation(location);
+        model.addAttribute("location", location);
+        return "main/single";
+    }
+*/
 
+    @ModelAttribute("locationMap")
+    public Map<Integer, String> locationMap() {
+        Map<Integer, String> locationMap = new LinkedHashMap<>();
+        locationMap.put(1, "1 구역");
+        locationMap.put(2, "2 구역");
+        locationMap.put(3, "3 구역");
+        return locationMap;
+    }
 }
